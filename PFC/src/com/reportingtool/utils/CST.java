fe7e070eb3210697a7baeab15946b54fd3fd98bc
Fernando <fernando6682@gmail.com>
2012-12-03 16:38:37 -0500

    Projects working on html page

--- PFC/src/com/reportingtool/utils/CST.java
+++ PFC/src/com/reportingtool/utils/CST.java
@@ -5,7 +5,7 @@ public class CST {
 	public static String PARTNERS_FILE = "Partners.xml";
 	public static String PROJECT_FILE = "Project.xml";
 	public static String PROJECTS_FILE = "projects.xml";
-
+	public static String REST_PATH = "PFC/rest/API/";
 	
 	
 }
