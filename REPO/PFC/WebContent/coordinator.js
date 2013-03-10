aa05744895a1025b9f7b2367b78a79eccde0d1f5
Fernando <fernando6682@gmail.com>
2013-03-10 19:04:59 -0400

    First demo working good!!!

--- REPO/PFC/WebContent/coordinator.js
+++ REPO/PFC/WebContent/coordinator.js
@@ -17,7 +17,7 @@
 						var partnerID=$(this).attr("id");
 						if( jQuery.inArray(partnerID, partnersArray) == -1 ){
 						var partnerName= $(this).children('name').text();	
-						$('#coordinatorPartners').append('<li><a href="#" id="triggerPartner" class="triggerPartner" partnerID="'+partnerID+'">'+ partnerName + '</a></li>');
+						$('#coordinatorPartners').append('<li><a href="#" id="triggerPartner" class="triggerPartner" partnerID="'+partnerID+'" partnername="'+partnerName+'">'+ partnerName + '</a></li>');
 						partnersArray.push(partnerID);
 						}
 						});//partner
@@ -42,16 +42,65 @@
      		
      		
     		$("#triggerPartner").live("click",function() {
+    			$('#topArticle').empty();
+    			var partnerID = $(this).attr('partnerid');
+    			var partnerName = $(this).attr('partnername');
+    			$('#topArticle').append('<h2><a>Partner '+partnerName+' taking part in WPs:</a></h2>');
     			$(xmlReport).find('subreport').each(function(){
-					
-					var reportDate=$(this).attr("date");
 					var reportWP=$(this).attr("WP");
-					var reportPartner=$(this).attr("WP");
-					$('#wpArticle').append('<h2><a href="#">'+ reportDate + '|'+reportWP+'|'+reportPartner+'</a></h2>');
+					var partnerWP=$(this).attr("partner");
+						if(partnerID == partnerWP){
+						$('#topArticle').append('<h4><a href="#" id="triggerWP" partnerID="'+partnerID+'" partnername="'+partnerName+'" wp="'+reportWP+'">'+reportWP+'</a></h4>');
+						}
 					});//workpackage
+    	        $('#topArticleContainer').show();
+
+    		   });
+    		
+    		$("#triggerWP").live("click",function() {
+    			$('#midArticle').empty();
+    			$('#secondArticleContainer').hide();
+    			var partnerID = $(this).attr('partnerid');
+    			var partnerName = $(this).attr('partnername');
+    			var wp=$(this).attr('wp');
+    			$('#midArticle').append('<h2><a>Reports by Partner '+partnerName+' for WP '+wp+'</a></h2>');
+    			$(xmlReport).find('subreport').each(function(){
+					var reportWP=$(this).attr("WP");
+					var partnerWP=$(this).attr("partner");
+					var reportDate=$(this).attr("date");
+						if(partnerID == partnerWP && reportWP == wp){
+						$('#midArticle').append('<h4><a href="#" id="triggerReport" partnerID="'+partnerID+'" partnername="'+partnerName+'" wp="'+reportWP+'" reportdate="'+reportDate+'">'+reportDate+'</a></h4>');
+						}
+					});
     		   });
      		
-     		
+    		$("#triggerReport").live("click",function() {
+    			$('#secondArticle').empty();
+    			var partnerID = $(this).attr('partnerid');
+    			var date = $(this).attr('reportdate');
+    			var wp=$(this).attr('wp');
+    			var partnerName = $(this).attr('partnername');
+    			$(xmlReport).find('subreport').each(function(){
+					var reportWP=$(this).attr("WP");
+					var partnerWP=$(this).attr("partner");
+					var reportDate=$(this).attr("date");
+						if(partnerID == partnerWP && reportWP == wp && reportDate==date){
+						var expenses= $(this).children('expenses').text();	
+						var status= $(this).children('status').text();	
+						var feedback= $(this).children('feedback').text();	
+						var flag= $(this).children('flag').text();
+						var explanation= $(this).children('explanation').text();
+						$('#secondArticle').append('<h2><a>Report by Partner '+partnerName+' for WP '+wp+' on date '+date+'</a></h2>');
+						$('#secondArticle').append('<h4><a>Expenses:'+expenses+'</a></h4>');
+						$('#secondArticle').append('<h4><a>Status:'+status+'</a></h4>');
+						$('#secondArticle').append('<h4><a>Feedback:'+feedback+'</a></h4>');
+						$('#secondArticle').append('<h4><a>Flag:'+flag+'</a></h4>');
+						$('#secondArticle').append('<h4><a>Explanation:'+explanation+'</a></h4>');
 
-     		
+
+						}
+					});
+    	        $('#secondArticleContainer').show();
+
+    		});
      		
